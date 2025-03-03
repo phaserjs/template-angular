47159ece38e8bb8844d33ffb89a8edc2be6c5d19
MBDesu <39097222+MBDesu@users.noreply.github.com>
2025-03-03 04:41:34 -0500

    Update EventBus.ts
    Vue -> Angular

--- src/game/EventBus.ts
+++ src/game/EventBus.ts
@@ -1,5 +1,5 @@
 import { Events } from 'phaser';
 
-// Used to emit events between Vue components and Phaser scenes
+// Used to emit events between Angular components and Phaser scenes
 // https://newdocs.phaser.io/docs/3.70.0/Phaser.Events.EventEmitter
 export const EventBus = new Events.EventEmitter();
